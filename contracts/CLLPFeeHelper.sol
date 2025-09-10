c74200a183ada17f0c15c1d1214bd73d6e70438a
Leon.Zhang <leon.zhang@tron.network>
2025-09-10 17:00:55 +0800

    add CLLPFeeHelper

---
+++ contracts/CLLPFeeHelper.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+// Copyright (C) 2024 PancakeSwap
+pragma solidity 0.8.26;
+
+import {ICLPoolManager} from "infinity-core/src/interfaces/ICLPoolManager.sol";
+import {PoolId} from "infinity-core/src/types/PoolId.sol";
+import {Tick} from "infinity-core/src/libraries/Tick.sol";
+import {CLPosition} from "infinity-core/src/libraries/CLPosition.sol";
+import {FullMath} from "infinity-core/src/libraries/FullMath.sol";
+import {FixedPoint128} from "infinity-core/src/libraries/FixedPoint128.sol";
+
+/// @notice Helper contract to get the LP fees for a given position
+/// @dev warning: the value calculated through this contract might not exactly match the actual value received due to the potential charge from hooks contract
+contract CLLPFeesHelper {
+    /// @notice Get the LP fees for a given position
+    /// @param poolManager The address of pool manager
+    /// @param id The pool id
+    /// @param owner The owner of the position, for positions held by CLPositionManager, it should be the CLPositionManager address
+    /// @param tickLower The lower tick of the position
+    /// @param tickUpper The upper tick of the position
+    /// @param salt The salt of the position, for positions created by CLPositionManager, it should be the corresponding NFT id
+    /// @return feesOwed0 The lp fees owed to the owner of the position in token0
+    /// @return feesOwed1 The lp fees owed to the owner of the position in token1
+    function getLPFees(
+        ICLPoolManager poolManager,
+        PoolId id,
+        address owner,
+        int24 tickLower,
+        int24 tickUpper,
+        bytes32 salt
+    ) external view returns (uint256 feesOwed0, uint256 feesOwed1) {
+        (, int24 tickCurrent,,) = poolManager.getSlot0(id);
+        Tick.Info memory lower = poolManager.getPoolTickInfo(id, tickLower);
+        Tick.Info memory upper = poolManager.getPoolTickInfo(id, tickUpper);
+
+        (uint256 feeGrowthGlobal0X128, uint256 feeGrowthGlobal1X128) = poolManager.getFeeGrowthGlobals(id);
+
+        // calculate fee growth below
+        uint256 feeGrowthBelow0X128;
+        uint256 feeGrowthBelow1X128;
+        unchecked {
+            if (tickCurrent >= tickLower) {
+                feeGrowthBelow0X128 = lower.feeGrowthOutside0X128;
+                feeGrowthBelow1X128 = lower.feeGrowthOutside1X128;
+            } else {
+                feeGrowthBelow0X128 = feeGrowthGlobal0X128 - lower.feeGrowthOutside0X128;
+                feeGrowthBelow1X128 = feeGrowthGlobal1X128 - lower.feeGrowthOutside1X128;
+            }
+
+            // calculate fee growth above
+            uint256 feeGrowthAbove0X128;
+            uint256 feeGrowthAbove1X128;
+            if (tickCurrent < tickUpper) {
+                feeGrowthAbove0X128 = upper.feeGrowthOutside0X128;
+                feeGrowthAbove1X128 = upper.feeGrowthOutside1X128;
+            } else {
+                feeGrowthAbove0X128 = feeGrowthGlobal0X128 - upper.feeGrowthOutside0X128;
+                feeGrowthAbove1X128 = feeGrowthGlobal1X128 - upper.feeGrowthOutside1X128;
+            }
+
+            uint256 feeGrowthInside0X128 = feeGrowthGlobal0X128 - feeGrowthBelow0X128 - feeGrowthAbove0X128;
+            uint256 feeGrowthInside1X128 = feeGrowthGlobal1X128 - feeGrowthBelow1X128 - feeGrowthAbove1X128;
+
+            CLPosition.Info memory position = poolManager.getPosition(id, owner, tickLower, tickUpper, salt);
+
+            ///@dev Tho overflow is expected, it's technically possible users can lose their rewards if it hits type(uint128).max
+            feesOwed0 = FullMath.mulDiv(
+                feeGrowthInside0X128 - position.feeGrowthInside0LastX128, position.liquidity, FixedPoint128.Q128
+            );
+            feesOwed1 = FullMath.mulDiv(
+                feeGrowthInside1X128 - position.feeGrowthInside1LastX128, position.liquidity, FixedPoint128.Q128
+            );
+        }
+    }
+}
---
+++ deployTron/6.ts
@@ -0,0 +1,39 @@
+module.exports = async ({
+    getNamedAccounts,
+    deployments,
+    getChainId,
+    getUnnamedAccounts,
+  }) => {
+    const {deploy} = deployments;
+    const {deployer} = await getNamedAccounts();
+  
+    
+      // address _factoryV3,
+      // address _factoryV2,
+      // address _factoryStable,
+      // address _WETH9,
+      // ICLQuoter _clQuoter
+      // permit2: "0xF62F506D1FAA02E2354A9886B4A200496EE96F4B",
+      // weth9: "0xFB3B3134F13CCD2C81F4012E53024E8135D58FEE",
+      // v2Factory: "0x55F7D5D874A73F21C0851C62D374307985F440FC",
+      // v3Factory: "0xCAC0EE410E19A12CCE8805D5374BB60200FADD03",
+      // v3Deployer: "0x1234567890abcdef1234567890abcdef12345678", 
+      // v2InitCodeHash: "0x9dd9bfc2f6c1103a6c01d9c6a4044e4b8a9361f92df2728cdc3729922d56748e",
+      // v3InitCodeHash: "0xbdafe9a36668104a2d371dedf31aac9583722f1b2c2fe98dde229f50a1e81689",
+      // stableFactory: "0xabcdefabcdefabcdefabcdefabcdefabcdefabcd",
+      // stableInfo: "0x1234567890abcdef1234567890abcdef12345678",
+      // infiVault: "0xD033B0fD1B38D9a8f04a8C2Adc55b91c288930b1",
+      // infiClPoolManager: "0xD033B0fD1B38D9a8f04a8C2Adc55b91c288930b1",
+      // v3NFTPositionManager: "0x937A4ADE5E502DAAD2F640B58E0346FF5A13A01F",
+      // infiClPositionManager: "0xCF386E76CC4B99F164D4603C062EFE05A98D2E24",
+    const res = await deploy('CLLPFeesHelper', {
+      from: deployer,
+      gasLimit: 400000000,
+      tags: 'lumi6',
+    });
+    console.log(res)
+  };
+  
+  
+  module.exports.tags = ['lumi6'];
+  
